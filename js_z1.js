e7ccf0489ccc9b674ad19376c2e0d4154910f068
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-11-06 20:29:34 +0200

    Update js_z1.js

--- js_z1.js
+++ js_z1.js
@@ -5,11 +5,11 @@ console.clear()
 // if ( <cond> ) {
 // ..
 // }
-/*
+
 alert("hello") // pop-up dialog
 var x = 5
 alert(x)
-*/
+
 
 var a = parseInt(prompt("Enter a number:", 0))
 console.log(isNaN(a))
@@ -29,3 +29,76 @@ console.log(isNaN(a))
 //  ...
 //  }
 //  while (x < 0)
+//  also supports break + continue
+
+// part 1
+var x = parseInt(prompt("Enter 1st number:", 0))
+var y = parseInt(prompt("Enter 2nd number:", 0))
+if (isNaN(x) || isNaN(y)) {
+  console.error("must be 2 numbers ...")
+}
+else {
+  // $"" -- `${x}`
+  alert(`${x} + ${y} = ${x + y}`)
+  console.log(`${x} + ${y} = ${x + y}`)
+} 
+
+// part 2
+do {
+  var x = parseInt(prompt("Enter 1st number:", 0))
+} while (isNaN(x))
+do {
+  var y = parseInt(prompt("Enter 2nd number:", 0))
+} while (isNaN(y))
+  
+alert(`${x} + ${y} = ${x + y}`)
+console.log(`${x} + ${y} = ${x + y}`)
+
+switch(x) {
+  case 1:
+    // code block
+    break;
+  case 2:
+    // code block
+    break;
+  default:
+    // code block
+}
+
+/*
+try {
+  Block of code to try
+}
+catch(err) {
+  Block of code to handle errors
+}
+*/
+
+// type coercion
+var n1 = 5
+var n2 = "5"
+if (n1 == n2) {
+  console.log("equl")
+}
+else {
+  console.log("diff")
+}
+
+if (n1 === n2) {
+  console.log("equl")
+}
+else {
+  console.log("diff")
+}
+
+if (n1 != n2) {
+  console.log("equl")
+}
+else {
+  console.log("diff")
+}
+
+var line = prompt("Give me command:")
+
+//eval("alert('hi')") -- runs a js command from string
+eval(line)
