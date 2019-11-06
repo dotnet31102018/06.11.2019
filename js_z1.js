fa1c727594d16a6ea3a0a95413972776f6dd5d08
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-11-06 20:00:26 +0200

    Create js_z1.js

---
+++ js_z1.js
@@ -0,0 +1,31 @@
+
+console.clear()
+
+// and && or ||
+// if ( <cond> ) {
+// ..
+// }
+/*
+alert("hello") // pop-up dialog
+var x = 5
+alert(x)
+*/
+
+var a = parseInt(prompt("Enter a number:", 0))
+console.log(isNaN(a))
+
+// part 1:
+// input 2 numbers
+// print sum of numbers -- in alert and in console
+// if one of the numebrs is NaN console.error a message
+// part 2:
+// in a while-loop keep getting numbers till a number is entered and then print sum
+// example:
+//	while (x < 0) {
+//	...    
+//  }
+// or:
+//  do {
+//  ...
+//  }
+//  while (x < 0)
